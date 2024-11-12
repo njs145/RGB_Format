ef1a23d31e108b816856ab6152337c0501c75c15
JinSong <jsNam145@nextchip.com>
2024-11-12 14:06:36 +0900

    Sample Image 위치 변경

--- main.c
+++ main.c
@@ -39,19 +39,23 @@ int main(void)
     fp_test_RGB565 = open("Build/output/RGB565.RAW", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     fp_test_RGB555 = open("Build/output/RGB555.RAW", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-    BitMap_Extract_RGB_Data("SampleImage/BitMap/Bitmap_picture_DOG.bmp", &buff_RGB24, &size_RGB24);
+    BitMap_Extract_RGB_Data("SampleImage/Bitmap_picture_DOG.bmp", &buff_RGB24, &size_RGB24);
 
+    /* RGB565 추출 */
     size_RGB565 = (size_RGB24 / 24) * 16;
     buff_RGB565 = malloc(size_RGB565);
     BitMap_convert_RGB888toRGB565(TYPE_RGB565_LITTLE, size_RGB24, buff_RGB24, buff_RGB565);
     write(fp_test_RGB565, (char *)buff_RGB565,  size_RGB565);
     free(buff_RGB565);
 
+    /* RGB555 추출 */
     size_RGB555 = (size_RGB24 / 24) * 16;
     buff_RGB555 = malloc(size_RGB555);
     BitMap_convert_RGB888toRGB555(TYPE_RGB555_LITTLE, size_RGB24, buff_RGB24, buff_RGB555);
     write(fp_test_RGB555, (char *)buff_RGB555,  size_RGB555);
     free(buff_RGB555);
+
+    
 
     /* 맨 마지막 */
     free(buff_RGB24);
